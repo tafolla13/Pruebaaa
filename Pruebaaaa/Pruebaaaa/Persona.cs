3636f01b8324b15e45cf528c8640b9a2450e17fc
Tafolla <tafolla.1309@gmail.com>
2020-02-06 19:59:31 -0600

    Clase xd

---
+++ Pruebaaaa/Pruebaaaa/Persona.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pruebaaaa
+{
+    class Persona
+    {
+        private String nombre;
+        private int sueldo, horas;
+        public string Nombre { set; get; }
+        public int Sueldo { set; get; }
+        public int Horas { set; get; }
+    
+    public Persona (String nombre, int hora, int sueldo)
+        {
+            this.Nombre = nombre;
+            this.Sueldo = sueldo;
+            this.Horas = hora;
+        }
+    
+    }
+
+}
