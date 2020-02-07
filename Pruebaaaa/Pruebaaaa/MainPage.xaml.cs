92f9f311df1f98355561a3d821c4ebee90ee8758
Tafolla <tafolla.1309@gmail.com>
2020-02-06 20:32:08 -0600

    Termine

--- Pruebaaaa/Pruebaaaa/MainPage.xaml.cs
+++ Pruebaaaa/Pruebaaaa/MainPage.xaml.cs
@@ -22,7 +22,7 @@ namespace Pruebaaaa
         {
             String nombred = txtnombre.Text;
             int horasd = Convert.ToInt32(txtHoras.Text);
-            int sueldod = Convert.ToInt32(txtHoras.Text);
+            int sueldod = Convert.ToInt32(txtSueldo.Text);
             Persona pers = new Persona(nombred, sueldod, horasd);
             datos.Add(pers);
             txtnombre.Text = null;
@@ -33,12 +33,25 @@ namespace Pruebaaaa
 
         private void btnlimpiar_Clicked(object sender, EventArgs e)
         {
-
+            datos.Clear();
+            txtnombre.Text = null;
+            txtHoras.Text = null;
+            txtSueldo.Text = null;
         }
 
         private void btnBuscar_Clicked(object sender, EventArgs e)
         {
+            String buscar = txtnombre.Text;
+            bool encontrado = false;
+            foreach(Persona dato in datos)
+                if (dato.Nombre.Equals(buscar))
+                {
+                    txtHoras.Text = dato.Horas.ToString();
+                    txtSueldo.Text = dato.Sueldo.ToString();
+                    DisplayAlert("Tu sueldo", (dato.Sueldo * dato.Horas). ToString(), "Aceptar");
 
+
+                }
         }
     }
 }
