496d7ada58d342b1ea0734d56c5870b7cab757a1
Tafolla <tafolla.1309@gmail.com>
2020-02-06 19:47:28 -0600

    Crear clase Personas

---
+++ Pruebaaaa/Pruebaaaa/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Pruebaaaa
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAgregar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnlimpiar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBuscar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
